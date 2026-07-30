364ae5531c025e55b616eca4d45ce328b524e926
Bijan <bijan.nekouei@yahoo.com>
2026-07-30 09:51:50 +0200

    feat(chemistry): add curved arrow geometry utilities

--- components/chemistry/CurvedArrow/index.ts
+++ components/chemistry/CurvedArrow/index.ts
@@ -1,8 +1,12 @@
 export { default as CurvedArrow } from "./CurvedArrow";
 export { default as CurvedArrowPair } from "./CurvedArrowPair";
+
 export type {
   CurvedArrowProps,
   CurvedArrowTone,
   Point,
 } from "./types";
+
 export type { CurvedArrowPairProps } from "./CurvedArrowPair";
+
+export * from "./geometry";
